9250e6915b0b63e6c31c4c9d02bf30f9ef80d39d
MacStudent <macstudent@126-iMac31.local>
2019-03-04 21:32:28 -0500

    Ios App DEv test

--- Day1IosDev/ViewController.swift
+++ Day1IosDev/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var Multiplication: UIButton!
+    @IBOutlet weak var Division: UIButton!
+    @IBOutlet weak var Subtraction: UIButton!
+    @IBOutlet weak var Addition: UIButton!
+  
+    @IBOutlet weak var ResultMessage: UILabel!
+    @IBOutlet weak var txtNum1: UITextField!
+    @IBOutlet weak var txtNum2: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    
+    @IBAction func Multiplication(_ sender: Any) {
+        var   a = Int(txtNum1.text!)
+        var   b = Int(txtNum2.text!)
+        let c = a! * b!
+        ResultMessage.text = String(c)
+    }
+    @IBAction func Division(_ sender: Any) {
+        var   a = Int(txtNum1.text!)
+        var   b = Int(txtNum2.text!)
+        let c = a! / b!
+        ResultMessage.text = String(c)
+    }
+    @IBAction func Subtraction(_ sender: Any) {
+        var   a = Int(txtNum1.text!)
+        var   b = Int(txtNum2.text!)
+        let c = a! - b!
+        ResultMessage.text = String(c)
+    }
+    @IBAction func Addition(_ sender: UIButton) {
+    
+        var   a = Int(txtNum1.text!)
+        var   b = Int(txtNum2.text!)
+        let c = a! + b!
+        ResultMessage.text = String(c)
+    }
 }
+    
+
+
 
